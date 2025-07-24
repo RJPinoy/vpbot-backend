961fc5fc6a0e7edd650b88628328a010027616d5
Rejen <rejenkiller@live.fr>
2025-07-24 14:58:16 +0200

    :update: token is now sent to http cookie

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -4,6 +4,8 @@ namespace App\EventListener;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthenticationSuccessListener
 {
@@ -18,21 +20,29 @@ class AuthenticationSuccessListener
     * @param AuthenticationSuccessEvent $event
     */
    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
-   {
+    {
         $user = $event->getUser();
+        $token = $event->getData()['token'];
 
+        // Update last connected time
         if (method_exists($user, 'setLastConnected')) {
             $user->setLastConnected(new \DateTime());
             $this->em->flush();
         }
 
-        $event->setData([
-            'code' => $event->getResponse()->getStatusCode(),
-            'token' => $event->getData()['token'],
-            'user' => [
-                'id' => $user->getId(),
-                'email' => $user->getEmail(),
-            ],
-        ]);
-   }
+        // Add custom fields to the response data
+        $data = $event->getData();
+        $data['code'] = 200;
+        $data['token'] = $token;
+        $data['user'] = [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+        ];
+        $event->setData($data);
+
+        // Set token as HTTP-only cookie
+        $event->getResponse()->headers->setCookie(
+            Cookie::create('JWT', $token, new \DateTime('+1 hour'), '/', null, false, true, false, 'Lax')
+        );
+    }
 }
